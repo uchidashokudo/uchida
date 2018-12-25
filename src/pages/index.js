979409e5ad7627cf78de3b01d57a7691d9d86df7
Nathan Willson <willson.b.nathan@gmail.com>
2018-12-25 13:49:42 -0800

    Simplified homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import image1 from '../../static/img/donburi.jpg'
+import image2 from '../../static/img/fish.jpg'
+import image3 from '../../static/img/dish.jpg'
 
 export default class IndexPage extends React.Component {
   render() {
@@ -9,6 +13,11 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
+              <div>
+                <img className="homeImg" src={image1}/>
+                <img className="homeImg" src={image2} />
+                <img className="homeImg" src={image3} />
+              </div>
             </div>
           </div>
         </section>
@@ -16,18 +25,3 @@ export default class IndexPage extends React.Component {
     )
   }
 }
-
-IndexPage.propTypes = {
-  data: PropTypes.shape({}),
-}
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title,
-        description,
-      }
-    }
-  }
-`
