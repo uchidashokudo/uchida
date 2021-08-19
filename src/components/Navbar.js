f8ce73dab48daa24da9719da38fe5f300d0ec66c
Nathan Willson <willson.b.nathan@gmail.com>
2021-08-19 14:10:57 +0900

    new opening times

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,15 @@ const Navbar = class extends React.Component {
       <div id="mainLogo" className="container center">
         <div class="notice">
           <p>
-            👋&nbsp;&nbsp;Uchida Eatery will be closed until the middle of August. Sorry for the inconvenience and we look forward to seeing you soon.
+            👋&nbsp;&nbsp;Uchida Eatery is accepting ONLINE TAKE-OUT orders! Open Wednesday - Saturday. Pick up between 11:30am - 2:30pm.
+          </p>
+          <br/>
+          <p>
+            <a href="https://uchida-eatery.square.site/" target="_blank">Check out our new menu here.</a>&nbsp;&nbsp;🎉
+          </p>
+          <br/>
+          <p style={{fontSize: '0.9rem', fontStyle: 'italic'}}>
+            We aren't able to accept phone-orders and dining-in is not available at this time. We apologize for the inconvenience and appreciate the understanding.
           </p>
         </div>
         <Link to="/" title="Home">
@@ -53,19 +61,19 @@ const Navbar = class extends React.Component {
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
                 About
-               </Link>
+              </Link>
               <a className="navbar-item" href="https://uchida-eatery.square.site/">
                 Menu
               </a>
               <Link className="navbar-item" to="/contact">
                 Contact
-               </Link>
+              </Link>
             </div>
           </div>
         </div>
       </nav>
     </div>;
-  }
+ }
 }
 
 export default Navbar
